63937356604c6bc2ddb638281041eafbee563278
MIG239 <IgnatenkoMike@yandex.ru>
2017-10-04 11:42:05 +0300

    All data updated

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The code presented in file run_analysis.R.
 General description
 
 The code processes information regarding measurements of signals from Samsung watches accelerometer and gyroscope receiving
-average summary per activity type and per subject for Mean and Standard deviation values
+average summary per activity type and per subject for Mean and Standard deviation values for each variable
 
 The code represents the function which input parameter fname contains filename where to store the output dataset. 
 By default it was set as "tidy_data.csv". The output file separator is " ".
@@ -22,16 +22,6 @@ Working logic
 - Assigning names for these columns in both datasets
 - Adding columns with activity_id, subject_id and group name(test or train)
 - Adding activity name by activity_id
-- Transposing all variables containing measurement to one column(Key1) and measurement itlsef to another column
-- Splitting Key1 column into separate text and logic variables.   
-    Fast_Fourier_Transform (FFT) - by 1st symbol in the string where f corresponds to FFT and t to Time series.
-    Statistical_operation reflects whether mean or standard deviation calculated
-    Axis_name shows XYZ axis or Other measurement for values which were not converted to XYZ axis
-    Device_type - from which device the information was received Accelerometer/Gyroscope
-    Acceleration_type - Body or Gravity accelarion
-    Jerk_type - whether calculation based on jerk values or normal.
-    Magnitude reflects magnitude or other measure
 - Selecting only required columns excluding working columns and non-required
-- Transpose values of the column with name of statistical operation to new column names (mean and standard deviation)
-and measurement values to this columns
+- Grouping dataset per subject and activity name with calculating averages for each variable
 - Writing down the file
--- run_analysis.R
+++ run_analysis.R
@@ -46,47 +46,15 @@ run_analysis <- function(fname="tidy_data.csv"){
                      mutate(Observation_group="train", Subject_id=subject_train$V1, Activity_id=y_train$V1) %>%
                      ##Attaches activity type names to the file by activity_id column
                      merge(activity_labels, by.x="Activity_id", by.y="V1")) 
-  tide_data<-tide_data %>% 
-    ##All variables which contain measurements united in one column Key1 as row values
-    ##and all measurements in another called measurement
-    gather(key=Key1, value=measurement, names(tide_data[2:67])) %>% 
-    ##Divides Key1 column to factors coded in column value
-    ##Fast_Fourier_Transform (FFT) - by 1st symbol in the string where f corresponds to
-    ##FFT and t to Time series.
-    ##Statistical_operation reflects whether mean or standard deviation calculated
-    ##Axis_name shows XYZ axis or Other measurement for values which were not
-    ##converted to XYZ axis
-    ##Device_type - from which device the information was received Accelerometer/Gyroscope
-    ##Acceleration_type - Body or Gravity accelarion
-    ##Jerk_type - whether calculation based on jerk values or normal.
-    ##Magnitude reflects magnitude or other measure
-    mutate(Fast_Fourier_Transform=if_else(substr(Key1,1,1)=="f", TRUE, FALSE), 
-           Statistical_operation=if_else(grepl("mean", Key1), "Mean", "Standard deviation"), 
-           Axis_name=if_else(substr(Key1,nchar(Key1),nchar(Key1)) %in% c("X", "Y", "Z"), substr(Key1,nchar(Key1),nchar(Key1)),"Other"), 
-           Device_type=if_else(grepl("Acc", Key1), "Accelerometer", "Gyroscope"), 
-           Acceleration_type=if_else(grepl("Body", Key1), "Body acceleration", "Gravity acceleration"), 
-           Jerk_type=if_else(grepl("Jerk", Key1), "Jerk signal", "Routine signal"), 
-           Magnitude=if_else(grepl("Mag", Key1), "Signal magnitude", "Other measure") ) %>%
-    ##Renames column with activities
-    rename(Activity_type=V2, Measurement=measurement) %>% 
-    ##Sets column to be presented in preprocessed table
-    select(Observation_group, Subject_id, Activity_type, Fast_Fourier_Transform, 
-           Axis_name, Device_type, Acceleration_type, Jerk_type, Magnitude, 
-           Statistical_operation, Measurement)
-  
-  ##Creation of new data table with average of mean and standard deviation values per subject and activity type 
-  new_data<-tide_data %>% 
-    ##Creates groups activity type, subject, statistical operation and features of original variables
-    group_by(Subject_id, Activity_type, Fast_Fourier_Transform, 
-             Axis_name, Device_type, Acceleration_type, Jerk_type, Magnitude, 
-             Statistical_operation) %>% 
-    ##Calculates average figures per activity, subject and statistical operation
-    summarize(Average_value=mean(Measurement)) %>% 
-    ##Substitutes variables Statistical_operation and Measurement by Mean and Standard
-    ##deviation variables containing its averages by activity and subject
-    spread(Statistical_operation, Average_value)
-  ##Writes information from new summarized table to the file which name was
-  ##specified in fname variable which is input of function run_analysis
-  write.table(new_data, fname, row.name=FALSE)
- 
+  tide_data<-tide_data %>%
+    ##Selects only required columns
+    select(Subject_id, V2, names(tide_data[2:67])) %>%
+    ##Renames columns to readable names
+    rename(Activity_name=V2) %>%
+    ##Creates groups by subject and activity name
+    group_by(Subject_id, Activity_name) %>%
+    ##Groups data with calculating average per group
+    summarize_all(mean)
+  ##Write down the dataset to the file with name stored in variable fname
+  write.table(tide_data, fname, row.name=FALSE)
   }
